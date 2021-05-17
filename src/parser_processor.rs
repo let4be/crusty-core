2d78d5d8808c9b54685b3b3ed750dd39a031cfa6
Sergey F <let4be@gmail.com>
2021-05-17 22:06:24 +0300

    improved parser processor logging

--- src/parser_processor.rs
+++ src/parser_processor.rs
@@ -5,8 +5,6 @@ use crate::{
     config,
 };
 
-use std::thread::JoinHandle;
-
 #[derive(Clone)]
 pub struct ParserProcessor {
     concurrency: usize,
@@ -21,39 +19,41 @@ impl ParserProcessor {
         (Self {concurrency: concurrency_profile.parser_concurrency, stack_size_bytes, rx}, tx)
     }
 
-    fn process(&self, _n: usize) -> Result<()> {
-        while let Ok(task) = futures_lite::future::block_on(self.rx.recv()) {
-            if task.res_tx.is_closed() {
-                continue;
+    fn process(&self, n: usize) -> PinnedFut {
+        TracingTask::new(span!(Level::INFO, n=n), async move {
+            while let Ok(task) = futures_lite::future::block_on(self.rx.recv()) {
+                if task.res_tx.is_closed() {
+                    continue;
+                }
+
+                let wait_time = task.time.elapsed();
+                let t = Instant::now();
+                let payload = task.payload;
+                let res = payload();
+                let work_time = t.elapsed();
+
+                let _ = futures_lite::future::block_on(task.res_tx.send(ParserResponse {
+                    res,
+                    wait_time,
+                    work_time
+                }));
             }
 
-            let wait_time = task.time.elapsed();
-            let t = Instant::now();
-            let payload = task.payload;
-            let res = payload();
-            let work_time = t.elapsed();
-
-            let _ = futures_lite::future::block_on(task.res_tx.send(ParserResponse {
-                res,
-                wait_time,
-                work_time
-            }));
-        }
-
-        Ok(())
+            Ok(())
+        }).instrument()
     }
 
     pub async fn go(self) -> Result<()>
     {
-        for h in (0..self.concurrency).into_iter().map(|n| -> Result<JoinHandle<Result<()>>>{
+        for h in (0..self.concurrency).into_iter().map(|n|{
             let h = std::thread::Builder::new().stack_size(self.stack_size_bytes).spawn({
                 let p = self.clone();
                 let n = n;
                 move || {
-                    p.process(n)
+                    let _ = futures_lite::future::block_on(p.process(n));
                 }
-            }).context("cannot spawn html parser thread")?;
-            Ok(h)
+            }).context("cannot spawn parser processor thread")?;
+            Ok::<_, Error>(h)
         }) {
             let _ = h?.join();
         }
