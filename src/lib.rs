02088f452ce4dd08d580ffa13921c324c8df33d2
Sergey F <let4be@gmail.com>
2021-05-19 11:43:43 +0300

    lib.rs cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
-mod crawler;
 mod prelude;
-pub mod config;
-pub use crawler::*;
 
-mod task_processor;
-mod task_scheduler;
+mod crawler;
+pub use crawler::*;
 mod parser_processor;
 pub use parser_processor::*;
 
+pub mod types;
+pub mod config;
 pub mod status_filters;
 pub mod load_filters;
 pub mod task_filters;
 pub mod expanders;
-pub mod types;
-mod hyper_utils;
 pub mod resolver;
+
+mod task_processor;
+mod task_scheduler;
+mod hyper_utils;
