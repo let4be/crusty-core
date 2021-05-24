f2184502a9a4f56e2b670b207cdc63d5b2eaa9b9
Sergey F <let4be@gmail.com>
2021-05-24 18:56:35 +0300

    updated examples

--- examples/simple-short/src/main.rs
+++ examples/simple-short/src/main.rs
@@ -1,11 +1,4 @@
-use crusty_core::{
-    ParserProcessor, CrawlingRules, CrawlingRulesOptions, Crawler, TaskExpander,
-    types::{
-        Job, JobCtx, Task, HttpStatus, JobStatus,
-        select::predicate::Name, select::document::Document
-    },
-    config,
-};
+use crusty_core::prelude::*;
 
 #[derive(Debug, Clone, Default)]
 pub struct JobState {
@@ -30,15 +23,10 @@ impl TaskExpander<JobState, TaskState> for DataExtractor {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let concurrency_profile = config::ConcurrencyProfile::default();
-    let pp = ParserProcessor::spawn(concurrency_profile, 1024 * 1024 * 32);
-
-    let networking_profile = config::NetworkingProfile::default().resolve()?;
-    let crawler = Crawler::new(networking_profile, &pp);
+    let crawler = Crawler::<AsyncHyperResolver>::new_default()?;
 
     let settings = config::CrawlingSettings::default();
-    let rules_opt = CrawlingRulesOptions::default();
-    let rules = CrawlingRules::new(rules_opt).with_task_expander(|| DataExtractor{} );
+    let rules = CrawlingRules::default().with_task_expander(|| DataExtractor{} );
 
     let job = Job::new("https://bash.im", settings, rules, JobState::default())?;
     for r in crawler.iter(job) {
@@ -47,6 +35,5 @@ async fn main() -> anyhow::Result<()> {
             println!("final job state: {:?}", r.context.job_state.lock().unwrap());
         }
     }
-
-    Ok(pp.join().await??)
+    Ok(())
 }
