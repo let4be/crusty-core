53839fbeaadd66f4b231a3ee03999842630e2b66
Sergey F <let4be@gmail.com>
2021-06-17 13:43:51 +0300

    reorganized lib.rs mod/use

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -3,21 +3,25 @@ pub use select;
 
 #[macro_use]
 extern crate lazy_static;
-
 #[macro_use]
 mod macro_helpers;
 
 mod internal_prelude;
 pub mod prelude;
 
+mod hyper_utils;
+mod task_processor;
+mod task_scheduler;
+
+pub mod config;
+pub mod resolver;
+pub mod types;
+
 mod crawler;
 pub use crawler::*;
 mod parser_processor;
 pub use parser_processor::*;
-
-pub mod config;
 pub mod status_filters;
-pub mod types;
 pub use status_filters::Filter as StatusFilter;
 pub mod load_filters;
 pub use load_filters::Filter as LoadFilter;
@@ -25,8 +29,3 @@ pub mod task_filters;
 pub use task_filters::Filter as TaskFilter;
 pub mod task_expanders;
 pub use task_expanders::Expander as TaskExpander;
-pub mod resolver;
-
-mod hyper_utils;
-mod task_processor;
-mod task_scheduler;
