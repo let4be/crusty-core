c20086e25311c5485bb415ee8407ad605aff5696
Sergey F <let4be@gmail.com>
2021-06-20 13:30:43 +0300

    (cargo-release) version 0.48.0

--- main/src/config.rs
+++ main/src/config.rs
@@ -245,7 +245,7 @@ impl Default for CrawlingSettings {
 			job_hard_timeout:          CDuration::from_secs(60),
 			job_soft_timeout:          CDuration::from_secs(30),
 			load_timeout:              CDuration::from_secs(10),
-			user_agent:                Some(String::from("crusty-core/0.47.1")),
+			user_agent:                Some(String::from("crusty-core/0.48.0")),
 			compression:               true,
 			custom_headers:            HashMap::new(),
 			max_response_size:         CBytes(1024 * 1024 * 2),
