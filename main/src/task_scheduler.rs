395fcdc04155fe83db4ee36abe08ce3537725300
Sergey F <let4be@gmail.com>
2021-06-29 12:32:46 +0300

    added todo for  root_task errors propagation, #13 (need to figure out a nice non hackish way)

--- main/src/task_scheduler.rs
+++ main/src/task_scheduler.rs
@@ -113,6 +113,11 @@ impl<JS: JobStateValues, TS: TaskStateValues, P: ParsedDocument> TaskScheduler<J
 			self.schedule(Arc::new(task));
 		}
 
+		// TODO: figure a nice way to propagate errors in root tasks to Job Finished status
+		/*if self.pages_pending < 1 && task_response.task.is_root() {
+
+		}*/
+
 		let _ = self.update_tx.send_async(task_response).await;
 	}
 
